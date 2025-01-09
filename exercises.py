e7917983c5e550333dec2dc2b99e676d99105e86
Tony Gadaleta <anthonygadaleta@gmail.com>
2025-01-09 17:05:34 -0500

    fixed some code

--- exercises.py
+++ exercises.py
@@ -3,12 +3,11 @@ class Game():
         self.turn = turn
         self.tie = tie
         self.winner = winner
-    
-    board = {
+        self.board = {
         'a1': None, 'b1': None, 'c1': None,
         'a2': None, 'b2': None, 'c2': None,
         'a3': None, 'b3': None, 'c3': None,
-    }
+        }
 
     def print_board(self):
         b = self.board
